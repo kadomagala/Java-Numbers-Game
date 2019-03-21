00bac6a6d4ad34d0e95afb5c436390b9c7f024b8
kamil <edgemond2@gmail.com>
2019-03-21 12:06:18 +0100

    Restructured main structure for strip, from HashMap to 2Dim array.

--- src/sample/Strip.java
+++ src/sample/Strip.java
@@ -4,44 +4,41 @@ import java.util.HashMap;
 import java.util.Random;
 
 public class Strip {
-    static public HashMap<Integer, Cordinates>  strip;
+    static public int[][] strip;
     public static NumberBean[] beans;
     public static int rowSize;
     public static int colSize;
     public static int numOfBeans;
 
-    public static void Init(int numOfBeans, int rowSize, int colSize){
+    public static void Init(int numOfBeans, int rowSize, int colSize) {
         beans = new NumberBean[numOfBeans];
         Strip.rowSize = rowSize;
         Strip.colSize = colSize;
         Strip.numOfBeans = numOfBeans;
-        strip = new HashMap<>();
+        strip = new int[rowSize][colSize];
+        for (int i = 0; i < rowSize; i++)
+            for (int j = 0; j < colSize; j++)
+                strip[i][j] = -1;
         Random r = new Random();
         Cordinates cordinates = new Cordinates(r.nextInt(rowSize), r.nextInt(colSize));
-        strip.put(0,cordinates);
+        strip[cordinates.getX()][cordinates.getY()] = 0;
         NumberBean zeroBean = new NumberBean();
         zeroBean.setValue(0);
         zeroBean.setCords(cordinates);
         zeroBean.isPrime = false;
-        for(int i = 1; i < numOfBeans; i++){
+        for (int i = 1; i < numOfBeans; i++) {
+            int value = r.nextInt(10 * numOfBeans) + 1;
 
-            int value;
+            Cordinates cords;
             do {
-                value = r.nextInt(10 * numOfBeans) + 1;
-            }while (strip.containsKey(value));
-
-
-            Cordinates cords = new Cordinates(r.nextInt(rowSize), r.nextInt(colSize));
-            strip.put(0,cords);
-            do{
                 cords = new Cordinates(r.nextInt(rowSize), r.nextInt(colSize));
-            }while(strip.containsValue(cords));
+            } while (strip[cords.getX()] [cords.getY()]!=-1);
             NumberBean bean = new NumberBean();
             bean.setCords(cords);
             bean.setValue(value);
             bean.isPrime();
             beans[i] = bean;
-            strip.put(value, cords);
+            strip[cords.getX()][cords.getY()] = value;
         }
     }
 
